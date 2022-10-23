2d6e69a22f61149d829daf26cf891abd1329e69a
Luckyy9 <18521387@gm.uit.edu.vn>
2022-10-23 11:42:24 +0700

    add layout more

--- pages/books/edit/[name].js
+++ pages/books/edit/[name].js
@@ -3,10 +3,12 @@ import { Breadcrumb, Col, Row } from 'antd';
 import { ReadOutlined } from "@ant-design/icons";
 import { useRouter } from "next/router";
 import EditBookContent from "../../../component/book/EditBook";
+import Chap from '../../../component/items/Chap'
 
 export default function EditBook(){
     const hello='book1'
     const router= useRouter()
+
     return(
         <div>
             <Main>
@@ -20,10 +22,20 @@ export default function EditBook(){
                         <Breadcrumb.Item > Edit</Breadcrumb.Item>
                     </Breadcrumb>
                     <Row>
-                        <Col span={18}>
+                        <Col span={16}>
                             <EditBookContent></EditBookContent>
                         </Col>
-                        <Col span={6}></Col>
+                        <Col span={8}>
+                            <h3 className="book__edit__book__chap--title">All chap</h3>
+                            <div>
+                                <Chap ></Chap>
+                                <Chap ></Chap>
+                                <Chap ></Chap>
+                                <Chap ></Chap>
+                                <Chap ></Chap>
+                                <Chap ></Chap>
+                            </div>
+                        </Col>
                     </Row>
                 </div>
             </Main>
@@ -31,6 +43,16 @@ export default function EditBook(){
             <style jsx>{`
                 .book__edit__book{
                     margin: 92px 24px 24px;
+                }
+
+                .book__edit__book__chap--title{
+                    font-weight: 600;
+                    font-size: 16px;
+                    line-height: 24px;
+                    color: #1F1E22;
+                    position:relative;
+                    display: inline-block;
+                    margin:12px 24px 4px 4px;
                 }
             `}</style>
 
